e2e3cbe2540a61a59b5ba9e7e93a4b2338fedb1f
sruthi-dasari <113626139+sruthi-dasari@users.noreply.github.com>
2022-12-24 16:57:59 +0530

    star icon is not getting toggled

--- src/components/AppointmentItem/index.js
+++ src/components/AppointmentItem/index.js
@@ -1,27 +1,32 @@
-import {Component} from 'react'
 import {format} from 'date-fns'
+import './index.css'
 
-class AppointmentItem extends Component {
-  render() {
-    const {appointmentDetails} = this.props
-    const {title, date, isStarred} = appointmentDetails
+const AppointmentItem = props => {
+  const {appointmentDetails, toggleIsStarred} = props
+  const {title, date, isStarred} = appointmentDetails
 
-    const starImgUrl = isStarred
-      ? 'https://assets.ccbp.in/frontend/react-js/appointments-app/filled-star-img.png'
-      : 'https://assets.ccbp.in/frontend/react-js/appointments-app/star-img.png'
+  const starImgUrl = isStarred
+    ? 'https://assets.ccbp.in/frontend/react-js/appointments-app/filled-star-img.png'
+    : 'https://assets.ccbp.in/frontend/react-js/appointments-app/star-img.png'
 
-    const formattedDate = format(new Date(date), 'dd MMMM yyyy, EEEE')
-
-    return (
-      <li className="list-item-container">
-        <div className="list-upper-container">
-          <p className="appointment-title">{title}</p>
-          <img src={starImgUrl} alt="star" />
-        </div>
-        <p className="appointment-date">{formattedDate}</p>
-      </li>
-    )
+  const onStarClick = id => {
+    console.log('star clicked')
+    toggleIsStarred(id)
   }
+
+  const formattedDate = format(new Date(date), 'dd MMMM yyyy, EEEE')
+
+  return (
+    <li className="list-item-container">
+      <div className="list-upper-container">
+        <p className="appointment-title">{title}</p>
+        <button className="star-btn" onClick={onStarClick} type="button">
+          <img src={starImgUrl} alt="star" className="star-icon" />
+        </button>
+      </div>
+      <p className="appointment-date">{formattedDate}</p>
+    </li>
+  )
 }
 
 export default AppointmentItem
